abcc7dde022a824df63084faa47ad836089a8806
Douglasayala <douglasayal a612@gmail.com>
2020-05-13 17:28:29 -0600

    he instanciado en clase Delete el scanner

--- src/demo_crud/Delete.java
+++ src/demo_crud/Delete.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 
 public class Delete {
     
+     Delete () throws SQLException{
+        Scanner leer = new Scanner(System.in);
+        ConexionCRUD utilerias = new ConexionCRUD();
+        System.out.println("<< ELIMINAR REGISTRO >>");
+        
     
     
 }
