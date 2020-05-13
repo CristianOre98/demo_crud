207b23582f62bb72db4859a3791957aecfcc558c
Douglasayala <douglasayal a612@gmail.com>
2020-05-13 17:32:18 -0600

    He agregado el ingreso del id

--- src/demo_crud/Delete.java
+++ src/demo_crud/Delete.java
@@ -11,6 +11,13 @@ public class Delete {
         ConexionCRUD utilerias = new ConexionCRUD();
         System.out.println("<< ELIMINAR REGISTRO >>");
         
+        System.out.println("Ingrese el id del registro");
+        String idEstudianteEliminar = leer.next();
+        //reingreso de datos para actualizar
+        String tabla ="tb_estudiante ";
+        String campos = "*";
+        String condicion = "id_estudiante = "+idEstudianteEliminar;
+        utilerias.desplegarRegistros(tabla, campos, condicion);
     
     
 }
