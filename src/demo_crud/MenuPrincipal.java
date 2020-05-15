f73cd350e719d7f95a980f2747f7b63a3df63896
Adonis <adonisbautista39@gmail.com>
2020-05-14 17:49:30 -0700

    Eh Agregado el Metòdo de Desplegar Menù

--- src/demo_crud/MenuPrincipal.java
+++ src/demo_crud/MenuPrincipal.java
@@ -8,5 +8,20 @@ public class MenuPrincipal {
       public static void main(String[] args) throws SQLException {
         desplegarMenu();
     }
-    
+        public static void desplegarMenu() throws SQLException{
+        Scanner opcionSeleccionada = new Scanner(System.in);
+        String opcionMenu;
+        //menu a deplegar
+        System.out.println("************");
+        System.out.println("|     REGISTRO DE ESTUDIANTE     |");
+        System.out.println("************");
+        System.out.println("| Opciones:                      |");
+        System.out.println("|          1. Crear registro     |");
+        System.out.println("|          2. Consultar registro |");
+        System.out.println("|          3. Actualizar registro|");
+        System.out.println("|          4. Eliminar registro  |");
+        System.out.println("|          5. Salir              |");
+        System.out.println("************");
+        System.out.println("Seleccionar opción: ");
+        opcionMenu = opcionSeleccionada.next();
 }
