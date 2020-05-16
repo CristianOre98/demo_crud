c109bf8380f8f3a7d16755b410b81e23a733380e
CristianOre98 <crisvlaore98@gmail.com>
2020-05-15 22:24:08 -0600

    he agregado los import Scanner

--- src/demo_crud/Update.java
+++ src/demo_crud/Update.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo_crud;
 
-/**
- *
- * @author Alejandro
- */
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Update {
     
 }
